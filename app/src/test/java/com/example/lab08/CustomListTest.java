62d92bf2b1cfae6b061206c1d341cc010df468b2
Hetang Mehta <hetangmehta03@gmail.com>
2025-10-31 16:29:51 -0600

    Implementing Failing Test for GetCityCount

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -31,6 +31,30 @@ public class CustomListTest {
 
     }
 
+    @Test
+    public void testGetCityCount(){
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        City edmonton = new City("edmonton", "AB");
+        list.addCity(calgary);
+        list.addCity(edmonton);
+
+        assertTrue(list.hasCity(calgary));
+        assertEquals(2, list.getCityCount());
+        list.deleteCity(calgary);
+        assertFalse(list.hasCity(calgary));
+        assertEquals(1, list.getCityCount());
+
+        assertTrue(list.hasCity(edmonton));
+        assertEquals(1, list.getCityCount());
+        list.deleteCity(edmonton);
+        assertFalse(list.hasCity(edmonton));
+        assertEquals(0, list.getCityCount());
+
+    }
+
+
+
 
 
 }
